f7a3b98c898e952a09e371c4681eed47fbe916ec
miskinole2002 <sadeufotsing2002@gmail.com>
2023-11-18 17:05:09 -0500

    accueil

---
+++ pages/accueil.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="../styles/william.css" />
+<?php
+
+?>
+  <h1><center>bienvenue</center></h1>
+<body>
+<form method="POST" action="../results/login.php" >
+    <label for="add" ><center><span class="julian">combien d adresse voulez vous ? </span></center> </label>
+    <br><center><input type="number" id="add" name="add" class="julian" min="0"></center>
+    
+   <center><button type="submit" class="julian"> confirmer </button></center> 
+</form>
+
+</body>?
